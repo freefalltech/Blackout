64bee17d05d7db043bd06fdc3bf72863348dd3c0
Freefall Tech <techfreefall@gmail.com>
2017-04-07 22:32:00 +0530

    Edit strings, main activity for turn network off

--- app/src/main/java/io/github/freefalltech/blackout/MainActivity.java
+++ app/src/main/java/io/github/freefalltech/blackout/MainActivity.java
@@ -1,6 +1,5 @@
 package io.github.freefalltech.blackout;
 
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.net.Uri;
@@ -9,7 +8,6 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -27,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         searchWifiText.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/helvetica.otf"));
         titleHeader.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/helvetica.otf"));
         subText.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/centurygothic.ttf"));
-        dimBrighntess();
+        dimBrightness();
     }
 
     public void searchWifi(View v){
@@ -37,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         rl.setVisibility(View.VISIBLE);
     }
 
-    private void dimBrighntess() {
+    private void dimBrightness() {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M&&!Settings.System.canWrite(this)) {
                 Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
@@ -47,19 +45,17 @@ public class MainActivity extends AppCompatActivity {
         }
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M||Settings.System.canWrite(this)) {
             Settings.System.putInt(this.getContentResolver(),
-                    Settings.System.SCREEN_BRIGHTNESS, 20);
+                    Settings.System.SCREEN_BRIGHTNESS, 0);
 
             WindowManager.LayoutParams lp = getWindow().getAttributes();
-            lp.screenBrightness =0.2f;// 100 / 100.0f;
+            lp.screenBrightness =0f;
             getWindow().setAttributes(lp);
 
         }
     }
 
     public void turnNetworkOff(View view) {
-        Intent intent = new Intent();
-        intent.setClassName("com.android.settings",
-                "com.android.settings.Settings$DataUsageSummaryActivity");
+        Intent intent = new Intent(Settings.ACTION_DATA_ROAMING_SETTINGS);
         startActivity(intent);
     }
 }
